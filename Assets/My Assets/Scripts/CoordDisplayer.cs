2ce509deda8f0bc148e6a15d1c22b8cdec4e693d
Thomas Moffet <pantherman1996@gmail.com>
2016-02-10 17:14:28 +1100

    Added a little debugging info.

---
+++ Assets/My Assets/Scripts/CoordDisplayer.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class CoordDisplayer : MonoBehaviour {
+
+	public GameObject obj;
+	Transform objTransform;
+	Text myText;
+
+	// Use this for initialization
+	void Start () {
+		objTransform = obj.GetComponent<Transform>();
+		myText = GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		myText.text = "(" + Mathf.Round(objTransform.position.x).ToString() + ", " + Mathf.Round(objTransform.position.z).ToString() + ")";
+	}
+}
