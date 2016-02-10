13bebf9d430b58d9b7dab51ee7ef3577b9b419c7
Thomas Moffet <pantherman1996@gmail.com>
2016-02-10 15:52:29 +1100

    Made the ability to move the camera around

---
+++ Assets/My Assets/Scripts/CameraControl.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraControl : MonoBehaviour {
+
+	public float defaultSpeed = 1.0f;
+	public float fastSpeed = 3.0f;
+	Transform myTransform;
+
+	// Use this for initialization
+	void Start () {
+		myTransform = GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float speed = defaultSpeed;
+		if (Input.GetAxis("Run") > 0) {
+			speed = fastSpeed;
+		}
+
+		// Play around with the Input gravity to get a good feel.
+		myTransform.position += Vector3.forward * Time.deltaTime * speed * Input.GetAxis("Vertical");
+		myTransform.position += Vector3.right * Time.deltaTime * speed * Input.GetAxis("Horizontal");
+	}
+}
